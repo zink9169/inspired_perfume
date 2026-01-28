c33f11bd00e8c453a250ea1375f4a8b214ce620a
zink9169 <117247853+zink9169@users.noreply.github.com>
2026-01-28 15:06:51 +0630

    Fixed 3

--- my-app/src/pages/AdminPage.jsx
+++ my-app/src/pages/AdminPage.jsx
@@ -2,7 +2,7 @@
 import React from "react";
 import { Routes, Route } from "react-router-dom";
 import AdminDashboard from "../components/Admin/AdminDashboard";
-import PrivateRoute from "../components/Layout/PrivateRoute";
+import PrivateRoute from "../components/Layout/privateRoute";
 import Container from "react-bootstrap/Container";
 
 const AdminPage = () => {
